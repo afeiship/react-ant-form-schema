7f1748d9d68d1e93bd9d343bf57a63be52d1e6fd
aric.zheng <1290657123@qq.com>
2025-10-19 08:13:39 +0800

    feat: initial fields

--- packages/example/src/app.tsx
+++ packages/example/src/app.tsx
@@ -1,7 +1,7 @@
 import ReactAntdFormSchema from '@jswork/react-ant-form-schema/src/main';
 import React, { useEffect } from 'react';
 import { Button, Form } from 'antd';
-import { NiceFormMeta } from '@ebay/nice-form-react';
+import NiceForm, { NiceFormMeta } from '@ebay/nice-form-react';
 
 function App() {
   const [form] = Form.useForm();
@@ -9,12 +9,14 @@ function App() {
   const meta: NiceFormMeta = {
     columns: 1,
     viewMode: false,
+    rowGap: 10,
     initialValues: {
       username: 'admin-init',
       password: '123456-initial',
+      cities: [],
     },
     fields: [
-      { key: 'username', label: 'User Name' },
+      { key: 'username', label: 'User Name', help: 'Please enter your username', required: true },
       { key: 'password', label: 'Password', widget: 'password' },
       {
         key: 'cities',
@@ -44,12 +46,15 @@ function App() {
     <div className="m-10 p-4 shadow bg-gray-100 text-gray-800 hover:shadow-md transition-all">
       <div className="badge badge-warning absolute right-0 top-0 m-4">Build Time: {BUILD_TIME}</div>
       <ReactAntdFormSchema
-        className="mx-auto wp-8 p-5 bg-slate-200 rounded-lg"
+        className="mx-auto wp-9 p-5 bg-slate-200 rounded-lg"
         footerClassName="mb-0"
         loading={loading}
         form={form}
         meta={meta}
         onFinish={(values) => console.log(values)}>
+        <NiceForm
+          meta={{ fields: [{ key: 'ext-single-field', widgetProps: { placeholder: 'Single Field' } }] }}
+        />
         <Button type="primary" htmlType="submit">
           Submit
         </Button>
